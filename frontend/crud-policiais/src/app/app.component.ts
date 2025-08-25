a23b8eba2377c1906d376c304619dc528be1e3b3
Piter Lourencini <piter_elr@hotmail.com>
2025-08-25 12:36:07 -0300

    Adiciona navegação entre as rotas de cadastro e listagem

--- frontend/crud-policiais/src/app/app.component.ts
+++ frontend/crud-policiais/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; // Importe as diretivas aqui
+import { CommonModule } from '@angular/common'; // Módulo comum, se necessário
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule], // Adicione-as aqui
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
