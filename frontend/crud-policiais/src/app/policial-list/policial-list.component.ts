5effeba82ec750f1f7284fa2c3f8f3145eb44b50
Piter Lourencini <piter_elr@hotmail.com>
2025-08-25 11:28:34 -0300

    Criação do componente de listagem de policiais e exibição em tabela

--- frontend/crud-policiais/src/app/policial-list/policial-list.component.ts
+++ frontend/crud-policiais/src/app/policial-list/policial-list.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PoliciaService } from '../policia.service';
+import { CommonModule } from '@angular/common'; // Módulo para usar *ngIf e *ngFor
 
 @Component({
   selector: 'app-policial-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './policial-list.component.html',
   styleUrl: './policial-list.component.css'
 })
-export class PolicialListComponent {
+export class PolicialListComponent implements OnInit {
+  policiais: any[] = [];
+  loading: boolean = true;
+  errorMessage: string = '';
 
+  constructor(private policiaService: PoliciaService) { }
+
+  ngOnInit(): void {
+    this.carregarPoliciais();
+  }
+
+  carregarPoliciais(): void {
+    this.loading = true;
+    this.errorMessage = '';
+    this.policiaService.listarPoliciais()
+      .subscribe({
+        next: (data) => {
+          this.policiais = data;
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('Erro ao buscar policiais:', error);
+          this.errorMessage = 'Erro ao carregar a lista de policiais.';
+          this.loading = false;
+        }
+      });
+  }
 }
